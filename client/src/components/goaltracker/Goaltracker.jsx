801e2941e14a658f29bfb813e08aedfc54a32314
Keith <combs.cdi@gmail.com>
2023-08-16 14:42:53 -0400

    added goals and constants

---
+++ client/src/components/goaltracker/Goaltracker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Goaltracker = props => {
+  return (
+    <div>Goaltracker</div>
+  )
+}
+
+Goaltracker.propTypes = {}
+
+export default Goaltracker
---
+++ client/src/utils/constants.js
@@ -0,0 +1,23 @@
+export const WORKOUT = [{
+    arms: [
+        "biceps",
+        "triceps"
+    ],
+    shoulders:[
+        "lats"
+    ],
+    back:[
+        "upper", 
+        "lower"
+    ],
+    legs:[
+        "thigh", 
+        "hamstring",
+        "calves"
+    ]
+}];
+export const WORKOUT_TYPES =[{
+    hiit:{
+        
+    }
+}];
